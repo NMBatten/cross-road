fc2e19d6a748475f9076cccbe8c8e38acefb11bd
Nathaniel Batten <n@batten.cc>
2024-02-14 10:59:33 -0500

    Sets up event loop and initializes player.py

--- constants.py
+++ constants.py
@@ -2,9 +2,13 @@ from pygame.locals import *
 import pygame
 
 pygame.init()
-vec = pygame.math.Vector2
 
 
 class Constants:
 
-    HEIGHT
+    SCREEN_HEIGHT = 400
+    SCREEN_WIDTH = 500
+    FPS = 60
+    grass_green = (20, 200, 5)
+    river_blue = (5, 10, 230)
+    road_gray = (200, 200, 200)
--- main.py
+++ main.py
@@ -1,2 +1,24 @@
 from pygame.locals import *
-from constants import Constants
+from constants import Constants as con
+import pygame, time, sys
+
+pygame.init()
+vec = pygame.math.Vector2
+
+FramePerSec = pygame.time.Clock()
+
+displaysurface = pygame.display.set_mode((con.WIDTH, con.HEIGHT))
+pygame.display.set_caption("Don't get hit :)")
+
+
+while True:
+
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
+
+    displaysurface.fill(con.grass_green)
+
+    pygame.display.update()
+    FramePerSec.tick(con.FPS)
