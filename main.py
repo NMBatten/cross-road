89f959e0255f0bedc1d9c423eaf47bd0aab69521
Nathaniel Batten <n@batten.cc>
2024-02-15 10:16:09 -0500

    Populates the setup functions in main.py and continues to refine the objects

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from pygame.locals import *
 from constants import Constants as con
-import pygame, time, sys
+import pygame, time, sys, random
 from player import Player
-from objects import Slot, Truck
+from objects import Slot, Truck, RoadSlot
 
 pygame.init()
 vec = pygame.math.Vector2
@@ -14,14 +14,53 @@ pygame.display.set_caption("Don't get hit :)")
 
 p1 = Player(vec)
 
+# This variable tracks which slot is on top so the next slot can
+# be put on top of it
+top_slot = None
+
 class Groups:
+    all_sprites = pygame.sprite.Group()
+    deadly_obstacles = pygame.sprite.Group()
+    blocking_obstacles = pygame.sprite.Group()
     obstacles = pygame.sprite.Group()
+    slots = pygame.sprite.Group()
 
-def generate_slot():
+Groups.all_sprites.add(p1)
+
+def generate_slot(slot_type):
     # generates a new slot on top of the stack
-    pass
+    if slot_type == "grass":
+        slot = Slot(slot_pos())
+    elif slot_type == "road":
+        slot = RoadSlot(slot_pos())
+    top_slot = slot
+    Groups.all_sprites.add(slot)
+    Groups.slots.add(slot)
+
+def slot_pos():
+    return top_slot.rect.top + 15
+
+def generate_block():
+    # generates a random of slots of a certain type sandwiched
+    # between two strips of grass
+    generate_slot("grass")
+    for index in range(random.randint(3, 8)):
+        generate_slot("road")
+    generate_slot("grass")
 
 
+def setup():
+    # Sets up blocks until the screen is filled (or slightly over)
+    # Should initialize with some objects already in the middle
+    top_slot = Slot(vec((con.SCREEN_WIDTH / 2, con.SCREEN_HEIGHT-15)))
+    while top_slot.rect.top > - 100:
+        generate_block()
+    for slot in Groups.slots:
+        for index in range(random.randint(1, 4)):
+            truck = Truck(slot, random_start=True)
+            Groups.deadly_obstacles.add(truck)
+
+setup()
 
 while True:
 
--- objects.py
+++ objects.py
@@ -1,31 +1,55 @@
 from pygame.locals import *
 import pygame, random
 from constants import Constants as con
+from main import Groups
 
 class Slot(pygame.sprite.Sprite):
-    def __init__(self):
+    def __init__(self, pos):
         super().__init__()
         self.surf = pygame.Surface((con.SCREEN_WIDTH, 30))
-        self.surf.fill(con.road_gray)
-        self.rect = self.surf.get_rect()
+        self.surf.fill(con.grass_green)
+        self.rect = self.surf.get_rect(center = pos)
 
     def update(self):
         pass
 
-    def generate_obstacle(self, start_pos=None):
-        new_object = Truck(self)
+    def kill_condition(self):
+        return False
+
+class RoadSlot(Slot):
+    def __init__(self, pos):
+        super().__init__(pos)
+        self.surf.fill(con.road_gray)
+        self.object_speed = random.choice([-1, 1]) * con.TRUCK_SPEED
+
+    def generate_obstacle(self, random_start=False):
+        new_object = Truck(self, random_start)
+
+    def kill_condition(self, player):
+        # This checks whether or not the player should die based
+        # on the type of slot
+        hits = pygame.sprite.spritecollide(player, Groups.deadly_obstacles, False)
+        if hits:
+            return True
 
 
 class Truck(pygame.sprite.Sprite):
-    def __init__(self, slot, start_pos = None):
+    def __init__(self, slot, random_start):
         super().__init__()
-        self.surf = pygame.Surface((random.randint(20, 40), 20))
+        self.surf = pygame.Surface((random.randint(30, 50), 20))
         self.surf.fill(con.truck_color)
         self.rect = self.surf.get_rect()
+        self.slot = slot
 
         vec = pygame.math.Vector2
-
-        self.pos = vec((slot.rect.left, slot.rect.center.y -15))
+        if not random_start:
+            if self.slot.object_speed > 0:
+                xval = self.slot.rect.left
+            else:
+                xval = self.slot.rect.right
+            self.pos = vec((xval, slot.rect.center.y))
+        else:
+            self.pos = vec(( random.randint(0, con.SCREEN_WIDTH), slot.rect.center.y ))
 
     def update(self):
-        self.pos.x += con.TRUCK_SPEED
+        self.pos.x += self.slot.object_speed
